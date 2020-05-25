f666d747d66d8a1806d4fc16f27c00ca29e4a8f6
rajkush211 <rajkush211.rk@gmail.com>
2020-05-26 01:25:33 +0530

    ADDED : Quantity measurement user interface

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            quantityType: '',
+            quantityList: [],
+            quantityUnitsList: [],
+            unitType: '',
+            OutputUnitType: '',
+            path: '',
+            outputValue: [],
+            values: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:8081/quantity')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    quantityList: json
+                })
+            })
+        fetch('http://localhost:8081/quantity/LENGTH')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    quantityUnitsList: json,
+                    quantityType: 'LENGTH'
+                })
+            })
+    }
+
+    handleChange = (event) => {
+        fetch(`http://localhost:8081/quantity/${event.target.value}`)
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    quantityUnitsList: json
+                })
+            })
+
+    }
+
+    handleChangeInputUnit = (event) => {
+        this.setState({
+                unitType: event.target.value
+        })
+    } 
+
+    handleChangeOutputUnit = (event) => {
+        this.setState({
+                OutputUnitType: event.target.value
+        })
+    } 
+
+
+    handleChangeinput = async(event) => {
+        const val = event.target.value
+        await this.setState({
+            values: val
+        })
+        fetch("http://localhost:8081/quantity/conversion/" + this.state.OutputUnitType, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "Application/json"
+            },
+            body: JSON.stringify({"value": this.state.values, "quantityUnits": this.state.unitType})
+        }).then(res => res.json())
+            .then(json => {
+                console.log(json)
+                    this.setState({
+                        outputValue: json
+                    })
+                })   
+    }
+
+    render() {
+        const {quantityList, quantityUnitsList, outputValue} = this.state;
+        return (
+            <div className='mainDiv'>
+                <div className="App">
+                    <div className='Select'>
+                    <select  style={{ marginLeft: '18px',  marginRight: '18px', height: '40px', width: '93%'}}onChange={this.handleChange}>
+                        {quantityList.map(element => <option>{element}</option>)}
+                    </select>
+                    </div>
+                    <div className="selectDiv">
+                        <div className='box'>
+                            <input type="text" style={{ marginLeft:'267px'}} onChangeCapture={this.handleChangeinput}></input>
+                            <h4 style={{ marginTop:'-1px',  marginLeft:'6px',marginRight:'6px'}}> = </h4>
+                            <input value={JSON.stringify(outputValue)} type="text" style={{ marginLeft:'-2px'}}></input>
+                        </div>
+                    </div>
+                  
+                    <div className='box1'>
+                        <select style={{ width:'190px'}} onChange={this.handleChangeInputUnit}>{quantityUnitsList.map(units => <option>{units}</option>)}</select>
+                        <select style={{ marginLeft:'10px',width:'190px'}} onChange={this.handleChangeOutputUnit}>{quantityUnitsList.map(units => <option>{units}</option>)}</select>
+                    </div>
+                    <h3 style={{ marginLeft:'237px'}}>{this.state.quantityType}</h3>
+                </div></div>
+        );
+    }
 }
 
 export default App;
